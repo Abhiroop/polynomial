453abe99be99e1abb946572406a87a4fc81cd1ed
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-07-14 20:45:42 +0100

    Remove cabal directories

--- dist-newstyle/tmp/src-65201/base-compat-0.10.1/src/Data/Monoid/Compat/Repl.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE PackageImports #-}
-{-# OPTIONS_GHC -fno-warn-dodgy-exports -fno-warn-unused-imports #-}
--- | Reexports "Data.Monoid.Compat"
--- from a globally unique namespace.
-module Data.Monoid.Compat.Repl (
-  module Data.Monoid.Compat
-) where
-import "this" Data.Monoid.Compat
